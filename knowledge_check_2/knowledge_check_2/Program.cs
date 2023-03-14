637f6f9035372c54639cc0c782ac5bdb74530f45
rynsp8 <rynsp8@gmail.com>
2023-03-14 12:33:50 -0400

    Commitment of Code Louisville Knowledge Check 2

---
+++ knowledge_check_2/knowledge_check_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using knowledge_check_2;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<seat>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    Console.WriteLine($"Let's start with seat number {numberOfRecords}!");
+    Console.WriteLine($"What type of seat is seat{numberOfRecords}?");
+    string typeOfSeat = Console.ReadLine();
+    Console.WriteLine($"How many legs does your seat{numberOfRecords} have?");
+    int seatLegs = int.Parse(Console.ReadLine());
+    Console.WriteLine($"How soft is your seat{numberOfRecords}?");
+    string seatComfort = Console.ReadLine();
+    Console.WriteLine($"What color is your seat{numberOfRecords}?");
+    string seatColor = Console.ReadLine();
+
+    recordList.Add(new seat(typeOfSeat, seatLegs, seatComfort, seatColor));
+}
+
+// Print out the list of records using Console.WriteLine()
+
+foreach (var seat in recordList)
+{
+    Console.WriteLine("Seat: {0}, {1}, {2}, {3}", seat.seatType, seat.legs, seat.softness, seat.color);
+}
+
---
+++ knowledge_check_2/knowledge_check_2/seat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knowledge_check_2
+{
+    internal class seat
+    {
+        public string seatType { get; set; }
+        public int legs { get; set; }
+        public string softness { get; set; }
+        public string color { get; set; }
+
+        public seat(string seatType, int legs, string softness, string color)
+        {
+            this.seatType = seatType;
+            this.legs = legs;
+            this.softness = softness;
+            this.color = color;
+        }
+
+    }
+}
